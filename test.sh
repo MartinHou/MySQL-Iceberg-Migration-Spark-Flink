fc927b4bb3e31514ed581eb6bbf9adb0f8e7e66f
martin.hou <martin.hou@momenta.ai>
2023-12-12 18:29:40 +0800

    del useless

--- test.sh
+++
@@ -1,4 +0,0 @@
-export HADOOP_HOME=/mnt/data/martin.hou/hadoop-3.3.1/
-export HADOOP_CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath`
-export HADOOP_CONF_DIR=/mnt/data/martin.hou/hadoop-3.3.1/etc/hadoop/
-export HADOOP_USER_NAME=root
